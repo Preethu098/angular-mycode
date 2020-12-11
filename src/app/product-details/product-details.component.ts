faac4b7f3bbd02d8b564a5eac2a43f9826d05500
Preethu098 <preethunath@gmail.com>
2020-12-11 17:42:13 +0530

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,6 +10,7 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
   constructor
   (private route:ActivatedRoute,
